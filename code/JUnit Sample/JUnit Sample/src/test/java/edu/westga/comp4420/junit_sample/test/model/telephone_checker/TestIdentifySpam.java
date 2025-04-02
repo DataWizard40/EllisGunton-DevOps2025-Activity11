123cdaf4bae9e1359fa96b41e897042602a4d275
Richard E Gunton <rg00048@my.westga.edu>
2025-04-02 16:59:48 -0400

    Update unit tests to ensure 100% code coverage

--- code/JUnit Sample/JUnit Sample/src/test/java/edu/westga/comp4420/junit_sample/test/model/telephone_checker/TestIdentifySpam.java
+++ code/JUnit Sample/JUnit Sample/src/test/java/edu/westga/comp4420/junit_sample/test/model/telephone_checker/TestIdentifySpam.java
@@ -10,4 +10,24 @@ import edu.westga.comp4420.junit_sample.model.TelephoneChecker;
 
 class TestIdentifySpam {
 
+	@Test
+	void testInvalidPhoneNumber() {
+		TelephoneChecker checker = new TelephoneChecker();
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			boolean result = checker.identifySpam(999999L);
+		});
+	}
+	
+	@ParameterizedTest
+	@CsvSource({"1000000, false", "9990000000, true", "9000000000, true", 
+	"8880000000, true", "8000000000, true", "9110000000, true", 
+	"4110000000, true", "7700000000, false"})
+	void test(long number, boolean expectedResult) {
+		TelephoneChecker checker = new TelephoneChecker();
+		
+		boolean result = checker.identifySpam(number);
+		
+		assertEquals(expectedResult, result, "checking if number(" + number + ") was reported as spam");
+	}
 }
